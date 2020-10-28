9c4a22eed6a5c8539e1d8287b28cf5b0d6ae2836
micky387 <mickaelsaibi@free.fr>
2020-10-28 20:05:51 +0100

    rog3: Update kernel source from 17.0823.2009.98 vendor
    Change-Id: I23787ba487c6e6fbae0ee8bf1fb508d053e49c65

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -1458,6 +1458,12 @@ static int uvc_scan_chain_forward(struct uvc_video_chain *chain,
 		if (forward == prev)
 			continue;
 
+		if (forward->chain.next || forward->chain.prev) {
+			uvc_trace(UVC_TRACE_DESCR, "Found reference to "
+				"entity %d already in chain.\n", forward->id);
+			return -EINVAL;
+		}
+
 		switch (UVC_ENTITY_TYPE(forward)) {
 		case UVC_VC_EXTENSION_UNIT:
 			if (forward->bNrInPins != 1) {
