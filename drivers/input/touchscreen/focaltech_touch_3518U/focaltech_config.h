d1e35c36aa7660fe34658e1f5667513add9d1488
micky387 <mickaelsaibi@free.fr>
2020-11-29 08:10:06 +0100

    sm8250: Import 29.13.7.53 kernel source vendor
    Change-Id: Ic1fca9107c24711f0f10812cd6b05928e70acacb

--- drivers/input/touchscreen/focaltech_touch_3518U/focaltech_config.h
+++ drivers/input/touchscreen/focaltech_touch_3518U/focaltech_config.h
@@ -227,7 +227,7 @@
  * define your own fw_file, the sample one to be replaced is invalid
  * NOTE: if FTS_GET_MODULE_NUM > 1, it's the fw corresponding with FTS_VENDOR_ID
  */
-#define FTS_UPGRADE_FW_FILE                      "include/firmware/Zenfone7_VBC_D01_20201016_app.i"
+#define FTS_UPGRADE_FW_FILE                      "include/firmware/Zenfone7_VBE_D01_20201109_app.i"
 
 /*
  * if FTS_GET_MODULE_NUM >= 2, fw corrsponding with FTS_VENDOR_ID2
